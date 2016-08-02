cdaf57683975da4d4645a6be62cadcd793c05f5b
Shaikat0509 <shaikat0509@gmail.com>
2016-08-03 03:24:41 +0600

    js

---
+++ assets/dist/js/agg.js
@@ -0,0 +1,15 @@
+/**
+ * Agg v ()
+ * Copyright 2015-2016 Shariful
+ * Licensed under the MIT license
+ */
+$(document).ready(function() {
+
+  // Returns height of browser viewport
+  $(window).on('resize.windowscreen', function() {
+    $('.windowscreen').height($(this).height());
+  });
+
+  $(window).trigger('resize.windowscreen');
+
+});
